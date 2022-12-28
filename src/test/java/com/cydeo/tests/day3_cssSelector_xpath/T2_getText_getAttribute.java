b2c2c7fe14e917df69a7e20d95f01a753cd7f6b2
Bilge Ergün <blgergn@gmail.com>
2022-12-28 21:32:15 +0300

    using utilities class and practice

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T2_getText_getAttribute {
+
+    public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://login1.nextbasecrm.com/");
+
+        WebElement rememberMeButton = driver.findElement(By.className("login-item-checkbox-label"));
+        String expectedRememberMeButton="Remember me on this computer";
+        String actualRememberMeButton =rememberMeButton.getText();
+
+        if (expectedRememberMeButton.equals(actualRememberMeButton)){
+            System.out.println("First task PASSED!");
+        }else{
+            System.out.println("Sorry,FAILED!");
+        }
+
+        WebElement forgotPasswordButton = driver.findElement(By.className("login-link-forgot-pass"));
+        String expectedForgotPasswordButton="Forgot your password?";
+        String actualForgotPasswordButton=forgotPasswordButton.getText();
+
+        if (actualForgotPasswordButton.equalsIgnoreCase(expectedForgotPasswordButton)){
+            System.out.println("Congrats, second task is PASSED!");
+        }else{
+            System.out.println("Sorry, second task is FAILED!");
+        }
+
+    }
+}
