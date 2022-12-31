769351fcda499271d7dc7aebdef2e938b952fb59
Bilge Ergün <blgergn@gmail.com>
2023-01-01 00:47:14 +0300

    FindElements

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T4_findElements.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T4_findElements {
@@ -15,5 +18,17 @@ public class T4_findElements {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("https://practice.cydeo.com/abtest");
+
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+        System.out.println("allLinks.size() = " + allLinks.size());
+
+        //print out the text of the links. (iter)
+
+        for (WebElement each : allLinks) {
+
+            System.out.println("Text of Link: "+ each.getText());
+
+        }
     }
 }
