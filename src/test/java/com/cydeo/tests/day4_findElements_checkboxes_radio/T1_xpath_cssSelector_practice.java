704fbdb7ef0c40a67b2bcf0bbe13b576ebc9fef7
Bilge Ergün <blgergn@gmail.com>
2022-12-31 22:52:55 +0300

    using cssSelector and xPath

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -16,9 +16,26 @@ public class T1_xpath_cssSelector_practice {
         driver.get("https://practice.cydeo.com/forgot_password");
 
         WebElement homeLink = driver.findElement(By.cssSelector("a[class='nav-link'"));
+        WebElement homeLink2 = driver.findElement(By.cssSelector("a.nav-link"));
 
-        WebElement forgotPasswordHeader=driver.findElement(By.xpath("//*[@id=\"content\"]/div/h2"));
+        WebElement homeLink3 = driver.findElement(By.cssSelector("a.[href='/']"));
 
+        WebElement forgotPasswordHeader=driver.findElement(By.cssSelector("div.example>h2"));
+
+        WebElement forgotPasswordHeader2=driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
+        WebElement forgotPasswordHeader3=driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+
+        WebElement emailText=driver.findElement(By.cssSelector("label[for='email']"));
+        WebElement emailText2=driver.findElement(By.xpath("//label[@for='email']"));
+
+        WebElement inputbox=driver.findElement(By.xpath("//input[@type='text']"));
+        WebElement inputbox2=driver.findElement(By.xpath("//input[contains(@pattern,'a-z']"));
+
+        WebElement retrievePassword=driver.findElement(By.cssSelector("i.icon-2x"));
+
+        WebElement retrievePassword2=driver.findElement(By.xpath("//button[@id='form_submit']"));
+        WebElement cydeoText=driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+        WebElement cydeoText2=driver.findElement(By.cssSelector("div[style='text-align: center;']"));
 
     }
 
