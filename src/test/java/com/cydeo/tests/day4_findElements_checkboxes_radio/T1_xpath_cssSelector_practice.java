dcb60d836f9b934bfc8f59f38fa4494cbe7e9b06
Bilge Ergün <blgergn@gmail.com>
2022-12-31 23:12:00 +0300

    using cssSelector and xPath

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -9,36 +9,62 @@ public class T1_xpath_cssSelector_practice {
 
     public static void main(String[] args) {
 
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
-
+        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: XPATH and cssSelector Practices
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
+        //2. Go to https://practice.cydeo.com/forgot_password
         driver.get("https://practice.cydeo.com/forgot_password");
 
-        WebElement homeLink = driver.findElement(By.cssSelector("a[class='nav-link'"));
-        WebElement homeLink2 = driver.findElement(By.cssSelector("a.nav-link"));
+        //3. Locate all the WebElements on the page using XPATH and/or CSS
+        //locator only (total of 6)
+        //a. “Home” link
+        //Locate homeLink using cssSelector, class value
+        WebElement homeLink_ex1= driver.findElement(By.cssSelector("a[class='nav-link']"));
 
-        WebElement homeLink3 = driver.findElement(By.cssSelector("a.[href='/']"));
+        //Locate homeLink using cssSelector syntax #2
+        WebElement homeLink_ex2= driver.findElement(By.cssSelector("a.nav-link"));
 
-        WebElement forgotPasswordHeader=driver.findElement(By.cssSelector("div.example>h2"));
+        //Locate homeLink using cssSelector, href value
+        WebElement homeLink_ex3= driver.findElement(By.cssSelector("a[href='/']"));
 
-        WebElement forgotPasswordHeader2=driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
-        WebElement forgotPasswordHeader3=driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+        //b. “Forgot password” header
+        //Locate header using cssSelector: locate parent element and move down to h2
+        WebElement header_ex1 = driver.findElement(By.cssSelector("div.example > h2"));
 
-        WebElement emailText=driver.findElement(By.cssSelector("label[for='email']"));
-        WebElement emailText2=driver.findElement(By.xpath("//label[@for='email']"));
+        //Locate header using xpath, and using web elements text "Forgot Password"
+        //WebElement header_ex2 = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+        WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
 
-        WebElement inputbox=driver.findElement(By.xpath("//input[@type='text']"));
-        WebElement inputbox2=driver.findElement(By.xpath("//input[contains(@pattern,'a-z']"));
+        //c. “E-mail” text
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
 
-        WebElement retrievePassword=driver.findElement(By.cssSelector("i.icon-2x"));
+        //d. E-mail input box
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
 
-        WebElement retrievePassword2=driver.findElement(By.xpath("//button[@id='form_submit']"));
-        WebElement cydeoText=driver.findElement(By.xpath("//div[@style='text-align: center;']"));
-        WebElement cydeoText2=driver.findElement(By.cssSelector("div[style='text-align: center;']"));
+        //Locate inputBox using xpath contains method
+
+        //tagName[contains(@attribute,'value')]
+
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
+
+        //e. “Retrieve password” button
+        //button[@type='submit']
+        //button[@class='radius']
+        WebElement retrievePasswordBtn = driver.findElement(By.xpath("//button[@id='form_submit']"));
+
+        //f. “Powered by Cydeo text
+        WebElement poweredByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
+        //4. Verify all web elements are displayed.
+        System.out.println("homeLink_ex1.isDisplayed() = " + homeLink_ex1.isDisplayed());
+        System.out.println("header_ex1.isDisplayed() = " + header_ex1.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+        System.out.println("retrievePasswordBtn.isDisplayed() = " + retrievePasswordBtn.isDisplayed());
+        System.out.println("poweredByCydeoText.isDisplayed() = " + poweredByCydeoText.isDisplayed());
 
     }
-
-
-
 }
