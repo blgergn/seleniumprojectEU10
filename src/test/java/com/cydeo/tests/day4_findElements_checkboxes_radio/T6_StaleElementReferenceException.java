e7fe5c83713fe050e4ead8d56d56ab7bacf293e4
Bilge Ergün <blgergn@gmail.com>
2023-01-01 12:40:36 +0300

    StaleElementReferenceExpection

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -18,12 +18,13 @@ public class T6_StaleElementReferenceException {
         driver.get("https://practice.cydeo.com/abtest");
 
         WebElement cydeoLink = driver.findElement(By.xpath("//a[@href='https://cydeo.com/']"));
-
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
         Thread.sleep(2000);
         driver.navigate().refresh();
 
+        //We are refreshing the web element reference by re-assigning (re-locating) the web element
+        cydeoLink = driver.findElement(By.xpath("//a[@href='https://cydeo.com/']"));
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
 
