00737edeab07db910c94674f5e1de8ba98822623
Bilge Ergün <blgergn@gmail.com>
2023-01-02 23:15:23 +0300

    TestNG intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running...");
+
+    }
+}
