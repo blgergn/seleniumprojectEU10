530866e2e5082ce1ddfb3e6f4b02fda6d917568b
Bilge Ergün <blgergn@gmail.com>
2023-01-04 22:14:32 +0300

    Simple dropdown

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class T4_SimpleDropdowns {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+    @Test
+    public void simpleDropdownTest(){
+
+        Select simpleDropdown=new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+    }
+}
