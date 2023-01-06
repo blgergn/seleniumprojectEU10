c6ad4daf8a82c318641f4775dc3f12dd3391f1e7
Bilge Ergün <blgergn@gmail.com>
2023-01-06 22:02:09 +0300

    Alerts

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practices {
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test1(){
+
+        WebElement informationAlertButton=driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        informationAlertButton.click();
+
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+
+        WebElement information=driver.findElement(By.xpath("//p[.='You successfully clicked an alert']"));
+
+        String expectedResult="You successfully clicked an alert";
+        String actualResult=information.getText();
+
+        Assert.assertEquals(expectedResult,actualResult);
+
+
+
+    }
+}
