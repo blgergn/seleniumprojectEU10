ebe58c237f86d84feaef16b6020bbab8ddb5abd1
Bilge Ergün <blgergn@gmail.com>
2023-01-08 00:26:51 +0300

    day8 T1

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WebTable_Order_Verify {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void order_name_verify_test(){
+
+        driver.get("https://practice.cydeo.com/web-tables");
+
+
+
+
+    }
+}
