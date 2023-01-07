2d604323c852447952d645c5cb442eaf30f39b53
Bilge Ergün <blgergn@gmail.com>
2023-01-07 17:31:12 +0300

    window handling task

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,10 +1,12 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -16,12 +18,36 @@ public class T1_WindowHandling {
         driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://amazon.com");
+
     }
 
 
     @Test
     public void window_handling_test(){
+        driver.get("https://amazon.com");
+
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+        Set <String> allWindowsHandles=driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: "+driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+
+        }
+
+
+
+
 
     }
 }
