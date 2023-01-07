6a9c727210986056d2b3e9f99eaaf0e3adde2d48
Bilge Ergün <blgergn@gmail.com>
2023-01-07 17:34:52 +0300

    window handling task

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day7_webtables_utilities_javafaker;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -43,6 +44,11 @@ public class T1_WindowHandling {
                 break;
             }
 
+            //Assert: Title contains "Etsy"
+            String actualTitle=driver.getTitle();
+            String expectedTitle="Etsy";
+
+            Assert.assertEquals(expectedTitle,actualTitle);
         }
 
 
