5aaf6ade0e2044e1587157423683978ce245147e
Bilge Ergün <blgergn@gmail.com>
2023-01-08 20:15:30 +0300

    day8 T1

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,15 +19,22 @@ public class T1_WebTable_Order_Verify {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/web-tables");
+
     }
 
 
     @Test
     public void order_name_verify_test(){
 
-        driver.get("https://practice.cydeo.com/web-tables");
+        WebElement bobMartinCell=driver.findElement(By.xpath("//table[@id=ctl00_MainContent_orderGrid']//tr//td[.='Bob Martin']"));
 
+        //System.out.println("bobMartinCell.getText() = " + bobMartinCell.getText());
 
+        String expectedBobName= "Bob Martin";
+        String actualBobName= bobMartinCell.getText();
+
+        Assert.assertEquals(actualBobName,expectedBobName);
 
 
     }
