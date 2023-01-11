199226ff72e21413831c6d288bb8a6b96168160e
Bilge Ergün <blgergn@gmail.com>
2023-01-11 23:59:02 +0300

    added safari

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/t4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/t4_Config_Practice.java
@@ -1,7 +1,12 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,7 +17,10 @@ public class t4_Config_Practice {
 
     @BeforeMethod
     public void setupMethod() {
-        driver = WebDriverFactory.getDriver("chrome");
+
+        //We are getting the browserType dynamically from our configuration.properties file
+        String  browserType= ConfigurationReader.getProperty("browser");
+        driver = WebDriverFactory.getDriver(browserType);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
@@ -21,6 +29,15 @@ public class t4_Config_Practice {
 
     @Test
     public void google_search_test(){
+
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        searchBox.sendKeys("apple" + Keys.ENTER);
+
+        String expectedTitle= "apple - Google'da Ara";
+        String actualTitle=driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
 
     }
 }
--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
 
@@ -15,6 +16,9 @@ public class WebDriverFactory {
         }else if(browserType.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
+        }else if(browserType.equalsIgnoreCase("safari")){
+            WebDriverManager.safaridriver().setup();
+            return new SafariDriver();
         }else{
             System.out.println("Given browser type does not exist/or is not currently supported");
             System.out.println("Driver=null");
