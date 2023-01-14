dacc5e8037752ba2ab5169d8a09efb11cfe14ea3
Bilge Ergün <blgergn@gmail.com>
2023-01-15 00:29:55 +0300

    day10 T1

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -1,12 +1,28 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
 
     @Test
     public void registration_form_test(){
+       // Driver.getDriver() ---> driver.get(url)
+        Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
 
+        Faker faker=new Faker();
 
+        WebElement inputFirstName=Driver.getDriver().findElement(By.xpath("//input[@placeholder='first name']"));
+        inputFirstName.sendKeys(faker.name().firstName());
+
+        WebElement inputLastName=Driver.getDriver().findElement(By.xpath("//input[@placeholder='last name']"));
+        inputLastName.sendKeys(faker.name().lastName());
+
+        WebElement inputUsername=Driver.getDriver().findElement(By.xpath("//input[@placeholder='username']"));
+        inputUsername.sendKeys(faker.name().username());
     }
 }
