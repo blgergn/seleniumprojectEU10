d56da64f6b8e318755a5e208d90eafb1c729de51
Bilge Ergün <blgergn@gmail.com>
2023-01-14 16:16:14 +0300

    javaFaker

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -9,6 +9,7 @@ public class JavaFakerPractice {
     public void test1(){
         //Creating Faker object to reach methods
         Faker faker=new Faker();
+        //Faker faker=new Faker(new Locale("tr"));
         System.out.println("faker.name().firstName() = " + faker.name().firstName());
         System.out.println("faker.name().lastName() = " + faker.name().lastName());
 
