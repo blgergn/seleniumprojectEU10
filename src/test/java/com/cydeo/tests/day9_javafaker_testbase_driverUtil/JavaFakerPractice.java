e268bc4f415e1ae56d76384c89ffbca4b776c13d
Bilge Ergün <blgergn@gmail.com>
2023-01-14 16:11:56 +0300

    javaFaker

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -23,7 +23,9 @@ public class JavaFakerPractice {
 
         System.out.println("faker.bothify(\"##?-???\") = " + faker.bothify("##?-???"));
 
-        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard().replaceAll("-",""));
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact().replaceAll("Chuck Norris", "Muhtar"));
+
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
 
     }
     }
