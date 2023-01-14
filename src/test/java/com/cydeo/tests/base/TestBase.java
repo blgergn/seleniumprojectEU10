0c6282071e23153d3fbd827b212e4612976381ec
Bilge Ergün <blgergn@gmail.com>
2023-01-14 16:51:00 +0300

    testBase

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
 
-    public WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setupMethod() {
