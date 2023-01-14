964903d6a275859f5fc624d29fe2881a4612d35f
Bilge Ergün <blgergn@gmail.com>
2023-01-14 23:49:17 +0300

    day10 T1

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T1_Registration_Form {
+
+    @Test
+    public void registration_form_test(){
+
+
+    }
+}
