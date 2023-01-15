f014a81a082bb09b92a37d0b2f5cfe22f21ddc04
Bilge Ergün <blgergn@gmail.com>
2023-01-15 13:21:43 +0300

    Day10 T2 upload

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -1,11 +1,28 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
 
     @Test
     public void upload_test(){
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+        //Find some small file from your computer, and get the path of it.
+        String path ="/Users/bilge/Desktop/HTML Class/rogerfederer.jpg.webp";
+
+        //Upload the file.
+        WebElement fileUpload=Driver.getDriver().findElement(By.id("file-upload"));
+
+        BrowserUtils.sleep(2);
+        fileUpload.sendKeys(path);
+
+        WebElement uploadButton=Driver.getDriver().findElement(By.id("file-submit"));
+        uploadButton.click();
 
     }
 }
