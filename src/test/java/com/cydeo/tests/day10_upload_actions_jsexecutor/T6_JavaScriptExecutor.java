7faa82df2ced0234ff708d4481a081389ac05bd9
Bilge Ergün <blgergn@gmail.com>
2023-01-15 20:44:27 +0300

    Day10 T6 JavaScript

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class T6_JavaScriptExecutor {
@@ -9,6 +11,23 @@ public class T6_JavaScriptExecutor {
     public void javascript_executor_test1(){
 
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
+
+        //(JavascriptExecutor)Driver.getDriver().executeScript("");
+
+        //use below JavaScript method and scroll
+        JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
+
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,750)");
+        }
+
+
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+
 
 
     }
