35feae1c4d79e6d37d7d89d9d55b94911ab3a19f
Bilge Ergün <blgergn@gmail.com>
2023-01-15 13:01:11 +0300

    Day10 T2 upload

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T2_Upload_Practice {
+
+    @Test
+    public void upload_test(){
+
+    }
+}
