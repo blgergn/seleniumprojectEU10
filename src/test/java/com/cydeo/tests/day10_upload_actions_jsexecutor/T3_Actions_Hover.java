73c2f24e898e29cb29564782d92ac4c768e6c4ac
Bilge Ergün <blgergn@gmail.com>
2023-01-15 18:24:12 +0300

    Day10 T3 Actions

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -1,9 +1,11 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_Actions_Hover {
@@ -12,9 +14,25 @@ public class T3_Actions_Hover {
     public void hovering_test(){
         Driver.getDriver().get("https://practice.cydeo.com/hovers");
 
-        WebElement firstImage=Driver.getDriver().findElement(By.xpath("(//img[@src='/img/avatar-blank.jpg'])[1]"));
+        WebElement img1=Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2=Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3=Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        WebElement user1=Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
+        WebElement user2=Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
+        WebElement user3=Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
 
         Actions actions=new Actions(Driver.getDriver());
+
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img1).perform();
+        Assert.assertTrue(user1.isDisplayed());
+
+        actions.moveToElement(img2).perform();
+        Assert.assertTrue(user2.isDisplayed());
+
+        actions.moveToElement(img3).perform();
+        Assert.assertTrue(user3.isDisplayed());
 
 
 
