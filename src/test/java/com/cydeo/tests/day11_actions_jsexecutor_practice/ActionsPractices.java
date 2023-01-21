cab45ba7467205f9ff7f340fdd376be396c04f67
Bilge Ergün <blgergn@gmail.com>
2023-01-21 13:25:15 +0300

    Day11 ActionsPractices

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -9,26 +10,23 @@ import org.testng.annotations.Test;
 public class ActionsPractices {
 
     @Test
-    public void task_4_and_5_test(){
+    public void task_4_and_5_test() {
 
         Driver.getDriver().get("https://practice.cydeo.com/");
 
-        Actions actions=new Actions(Driver.getDriver());
+        Actions actions = new Actions(Driver.getDriver());
 
         //Locating CYDEO link to be able to pass in the actions method
-        WebElement cydeoLink=Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
 
         actions.moveToElement(cydeoLink).perform();
 
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
 
+        //  actions.moveToElement(homeLink).perform();
 
-
-
-
+        actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP, Keys.PAGE_UP, Keys.PAGE_UP).perform();
 
     }
-
-
-
 
 }
