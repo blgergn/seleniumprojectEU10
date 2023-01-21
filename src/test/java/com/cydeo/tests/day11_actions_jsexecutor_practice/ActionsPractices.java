f5dc5ec09ea215726e0cd2a4d6c2b53159291823
Bilge Ergün <blgergn@gmail.com>
2023-01-21 13:10:04 +0300

    Day11 ActionsPractices

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -1,6 +1,9 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -9,6 +12,20 @@ public class ActionsPractices {
     public void task_4_and_5_test(){
 
         Driver.getDriver().get("https://practice.cydeo.com/");
+
+        Actions actions=new Actions(Driver.getDriver());
+
+        //Locating CYDEO link to be able to pass in the actions method
+        WebElement cydeoLink=Driver.getDriver().findElement(By.linkText("CYDEO"));
+
+        actions.moveToElement(cydeoLink).perform();
+
+
+
+
+
+
+
     }
 
 
