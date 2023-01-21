dd1ea72688c229d5b3333658a3539ea01c6d3d40
Bilge Ergün <blgergn@gmail.com>
2023-01-21 12:58:14 +0300

    Day11 ActionsPractices

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void task_4_and_5_test(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/");
+    }
+
+
+
+
+}
