066c0bdbe11725e36419b2358bd8636dedfd1fba
Bilge Ergün <blgergn@gmail.com>
2023-02-06 00:50:29 +0300

    Day12 T1, T2 and T3

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -2,19 +2,54 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class POMPractices {
+
+    LibraryLoginPage libraryLoginPage;
 
     @Test
     public void required_field_error_message_test(){
 
         Driver.getDriver().get("https://library1.cydeo.com/");
 
-        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+       libraryLoginPage = new LibraryLoginPage();
 
         libraryLoginPage.signInButton.click();
 
-        libraryLoginPage.fieldRequiredErrorMessage.isDisplayed();
+        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void invalid_email_format_error_message_test(){
+        Driver.getDriver().get("https://library1.cydeo.com/");
+
+       libraryLoginPage=new LibraryLoginPage();
+
+        libraryLoginPage.inputUsername.sendKeys("lkfdngdlfgdflk");
+
+        libraryLoginPage.signInButton.click();
+
+        Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void library_negative_login_test(){
+        Driver.getDriver().get("https://library1.cydeo.com/");
+
+        libraryLoginPage=new LibraryLoginPage();
+
+        libraryLoginPage.inputUsername.sendKeys("anyway@dfsdfjsl.com");
+
+        libraryLoginPage.inputPassword.sendKeys("kjfkdfgkdngkdjnf");
+
+        libraryLoginPage.signInButton.click();
+
+        Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
     }
 }
